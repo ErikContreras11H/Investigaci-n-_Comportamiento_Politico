54ab31b3d16d097a43ddd34cfe3815054a56e8e4
ErikContreras11H <141360406+ErikContreras11H@users.noreply.github.com>
2023-08-04 13:23:49 -0600

    Add files via upload

---
+++ Script_Investigación.R
@@ -0,0 +1,51 @@
+######################################################
+#### Erik Contreras Hernandez
+#### *Script*
+###           Modelos econometricos de las Hipotises (H1,H2,H3,H4,H5)
+#####
+##### Covid y comportamiento lectoral, medidas de contencion y sus factores que las determina
+###
+######                           FLACSO- MEXICO
+########
+#######  Mexico 24 de julio de 2023
+######
+###### Director: Rodrigo Zalazar
+#################
+#########
+
+## Paqueteria requerida para correr los codigos
+install.packages("read")
+install.packages("effects")
+library(effects)
+install.packages("glmulti")
+install.packages("effects")
+library(glmulti)
+library(effects)
+library(readr)
+install.packages("ggplot2")
+library(ggplot2)
+library(car)
+install.packages("dplyr")
+library(dplyr)
+library(factoextra)
+install.packages("factoextra")
+library(cluster)
+library(simstudy)
+library(data.table)
+install.packages("tidyverse")
+library(tidyverse)
+library(PerformanceAnalytics)
+library(corrr)
+library(sandwich)
+library(lmtest)
+library(margins)
+library(interplot)
+install.packages("plm")
+library(readr)
+library(plm)## efectos fijos
+install.packages("AER")
+library(lubridate)
+
+
+
+
